88fd5edd9a73aae415dfd9ca0e5fe03fa29421cb
Manuel Farzini <manuel.farzini@gmail.com>
2025-05-21 14:41:41 +0200

    Fix incorrect @param in setWidth() doc comment

--- ecSource/include/emissionControl.h
+++ ecSource/include/emissionControl.h
@@ -270,7 +270,7 @@ class ecModulator {
   /**
    * @brief Set the width of the modulator.
    *
-   * @param[in] Frequency in Hz.
+   * @param[in] Width as a normalized value between 0 and 1 .
    */
   void setWidth(float width);
 
